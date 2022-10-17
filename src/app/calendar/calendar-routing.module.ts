5fcedd73cc65fd7204cec05469e25a7338576be8
PJHR <pedrojhrossi@gmail.com>
2022-10-17 13:09:23 -0400

    Heroku files config dependecies added

--- src/app/calendar/calendar-routing.module.ts
+++ src/app/calendar/calendar-routing.module.ts
@@ -3,7 +3,6 @@ import { RouterModule, Routes } from '@angular/router';
 import { AppointmentFormComponent } from './pages/appointment-form/appointment-form.component';
 import { AlreadyClientComponent } from './pages/already-client/already-client.component';
 import { ConfirmationComponent } from './pages/confirmation/confirmation.component';
-import { HomeComponent } from '../dicio/pages/home/home.component';
 
 const routes: Routes = [
   {
